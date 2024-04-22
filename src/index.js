e963f17c0db4e359978f7ef536870a0170fab86b
Suraj Rupnar <surajrupnar@Surajs-MacBook-Air.local>
2024-04-22 22:53:24 +0530

    03 React Essentials

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import ReactDOM from "react-dom/client";
+
+import App from "./App.js";
+import "./index.css";
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<App />);
